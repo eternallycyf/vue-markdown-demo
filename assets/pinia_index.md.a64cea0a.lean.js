578d1676bcd3cf3bd171ce3bd86cbbaace7e1c7f
eternallycyf <a969475322@gmail.com>
2022-09-26 16:12:20 +0800

    Updates

---
+++ assets/pinia_index.md.a64cea0a.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,g as a}from"./app.22d6e044.js";const t={components:{}},p='{"title":"手写 pinia","description":"","frontmatter":{"map":{"path":"/pinia","realPath":"src/pinia/index.zh-CN.md"}},"relativePath":"pinia/index.md","lastUpdated":1664179926287}',o=a('',9);t.render=function(a,t,p,e,c,u){return n(),s("div",null,[o])};export default t;export{p as __pageData};
--- assets/zh_pinia_index.md.06ecc151.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as s,g as a}from"./app.12c58a71.js";const t={components:{}},p='{"title":"手写 pinia","description":"","frontmatter":{"map":{"path":"/pinia","realPath":"src/pinia/index.en-US.md"}},"relativePath":"pinia/index.md","lastUpdated":1662297313564}',o=a('',9);t.render=function(a,t,p,e,c,u){return n(),s("div",null,[o])};export default t;export{p as __pageData};
